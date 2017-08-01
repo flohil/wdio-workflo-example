01daeb9659bfbc4077a58ec013c4df4905fa731f
Florian Hilbinger <florian.hilbinger@hotmail.com>
2017-08-01 18:49:16 +0200

    finished verification result handling

--- system_test/src/specs/homepage.spec.ts
+++ system_test/src/specs/homepage.spec.ts
@@ -6,4 +6,18 @@ Feature("Homepage", {}, () => {
       })
     })
   })
+  Story("1.2", "Failing story", {}, () => {
+    Given("a user opens the google homepage", () => {
+      When("the user observes the page title", () => {
+        Then(1, "This spec fails")
+      })
+    })
+  })
+  Story("4.4", "Another story", {}, () => {
+    Given("a user opens the google homepage", () => {
+      When("the user observes the page title", () => {
+        Then(1, "the correct page title should be displayed")
+      })
+    })
+  })
 })
