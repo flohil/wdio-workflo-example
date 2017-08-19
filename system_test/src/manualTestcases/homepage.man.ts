20eede53622f80f110db714a4a65662796b88942
Florian Hilbinger <florian.hilbinger@hotmail.com>
2017-08-19 16:08:43 +0200

    added ts support for manual test cases

--- system_test/src/manualTestcases/homepage.man.ts
+++ system_test/src/manualTestcases/homepage.man.ts
@@ -1,16 +1,9 @@
 // const homepage: Workflo.IManualTestcaseResults = {
-/*let homepage: Workflo.IManualTestcaseResults = {
-  "5.5": {
-    1: {result: true, date: '16.08.2017', comment: 'only fails in internet explorer'},
-    2: {result: false, date: '16.08.2017'}
-  }
-}*/
-
-// export default homepage
-
-module.exports = {
+let homepage: Workflo.IManualTestcaseResults = {
   "5.5": {
     1: {result: true, date: '16.08.2017'},
-    2: {result: false, date: '16.08.2017'}
+    2: {result: false, date: '16.08.2017', comment: 'only fails in internet explorer'}
   }
 }
+
+export default homepage
