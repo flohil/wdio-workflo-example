170421f679142fe3415167fc694af6e1452ccb67
Florian Hilbinger <florian.hilbinger@hotmail.com>
2019-01-21 18:55:04 +0100

    added manual results folder

---
+++ system_test/src/manual_results/npmjs.man.ts
@@ -0,0 +1,9 @@
+// const homepage: Workflo.IManualTestcaseResults = {
+const homepage: Workflo.IManualTestcaseResults = {
+  "5.5": {
+    1: { result: true, date: '2018-12-24' },
+    2: { result: false, date: '2018-12-24', comment: 'only fails in internet explorer' }
+  }
+};
+
+export default homepage;
